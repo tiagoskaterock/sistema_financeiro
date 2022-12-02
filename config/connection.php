fbeec5fbf14d909b3401d83bf7337d746b037d35
tiago <tiagolemespalhano@gmail.com>
2022-12-01 21:58:56 -0300

    Testando conexão

--- config/connection.php
+++ config/connection.php
@@ -1,3 +1,18 @@
 <?php
 
-$pdo = new PDO("mysql:dbname=DATABASE;host=SERVERNAME", "USERNAME", "PASSWORD");
+data_default_timezone_set('America/Sao_Paulo');
+
+$database = 'financeiro';
+
+$servername = 'localhost';
+
+$username = 'tiago';
+
+$password = '123';
+
+try {
+	$pdo = new PDO("mysql:dbname=$database;host=$servername", "$username", "$password");	
+} 
+catch (Exception $e) {
+	echo 'Não foi possível conectar ao banco de dados. <br><br>' . $e;
+}
--- config/stuff.php
+++ config/stuff.php
@@ -5,13 +5,3 @@ define('APP_NAME', 'Sistema Financeiro');
 define('URL', 'http://localhost/cupcake_software_projects/sistema_financeiro_php_pdo');
 
 define('ADMIN_EMAIL', 'admin@admin.com');
-
-
-// Database connection
-define('DATABASE', 'financeiro');
-
-define('SERVERNAME', 'localhost');
-
-define('USERNAME', 'tiago');
-
-define('PASSWORD', '123');
