49d8a136980ce80bc1274f2ee3ca90be3e64c205
tiago <tiagolemespalhano@gmail.com>
2022-12-03 00:50:53 -0300

    Janela modal do perfil

--- admin/dashboard/modal.php
+++ admin/dashboard/modal.php
@@ -12,27 +12,30 @@
       <!-- Modal body -->
       <div class="modal-body">
        <form method="post" action="#">
+
          <div class="form-group">
-           <label for="exampleInputEmail1">Email address</label>
-           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+           <label for="nome">Nome</label>
+           <input type="text" class="form-control" name="nome" aria-describedby="Nome" placeholder="Nome">
          </div>
+
          <div class="form-group">
-           <label for="exampleInputPassword1">Password</label>
-           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+           <label for="email">Email</label>
+           <input type="email" class="form-control" name="email" placeholder="Email">
          </div>
-         <div class="form-check">
-           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-           <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+         <div class="form-group">
+           <label for="senha">Senha</label>
+           <input type="password" class="form-control" name="senha" placeholder="******">
          </div>
-         <button type="submit" class="btn btn-primary">Submit</button>
+
+         <button type="submit" class="btn btn-primary">Salvar</button>
        </form>
            
       </div>
 
       <!-- Modal footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Fechar</button>
       </div>
 
     </div>
