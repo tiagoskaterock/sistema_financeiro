f9e633e7dfd71b54130975628c1f168f27c2ebbf
tiago <tiagolemespalhano@gmail.com>
2022-12-04 15:20:39 -0300

    Verificando Validações

--- admin/php/editar-perfil.php
+++ admin/php/editar-perfil.php
@@ -8,8 +8,12 @@ $email = escape($_POST['email']);
 $senha = escape($_POST['senha']);
 $user_id = $_POST['user_id'];
 
-// confere se existem mais usuários com mesmo email
-$sql = sql("SELECT count(email) FROM usuarios WHERE email = '$email'");
+// id de usuário atual 
+$user_atual = sql("SELECT id FROM usuarios WHERE id = '$user_id'");
+$user_atual_id = $user_atual['id'];
+
+// confere se existem mais usuários diferentes com o mesmo email
+$sql = sql("SELECT count(email) FROM usuarios WHERE email = '$email' AND id <> '$user_atual_id'");
 $registros_total = $sql["count(email)"];
 
 if ($registros_total > 0) {
