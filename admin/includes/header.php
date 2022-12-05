6706746c030989449aac5a4cafef5dde779b0cec
tiago <tiagolemespalhano@gmail.com>
2022-12-05 00:38:21 -0300

    Mostando view de níveis

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,34 +1,53 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['logado'])) {
+  echo '<script>window.location="' . URL .  '/login/"</script>';
+}
+
+// dados do usuário
+$user_id = $_SESSION['user_id'];
+
+$usuario = sql("SELECT * FROM usuarios WHERE id = '$user_id'");
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <?= utf8() ?>
   <?= viewport() ?>
   <title><?= APP_NAME ?> | Dashboard</title>
 
-  <?php favicon() ?>
+  <?php 
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
-  <!-- Ajax -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  css("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback");
+
+  favicon();
+
+  css(URL . '/admin/plugins/fontawesome-free/css/all.min.css');
+
+  css("https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css");
+
+  css(URL . "/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css");
+
+  css(URL . "/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css");
+
+  css(URL . "/admin/plugins/jqvmap/jqvmap.min.css");
+
+  css(URL . "/admin/dist/css/adminlte.min.css");
+
+  css(URL . "/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css");
+
+  css(URL . "/admin/plugins/daterangepicker/daterangepicker.css");
+
+  css(URL . "/admin/plugins/summernote/summernote-bs4.min.css");
+
+  js("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+
+  ?>
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
