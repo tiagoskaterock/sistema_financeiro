b3bdc02ec0a6131df2703fa422fbb402f80edbe3
Tiago Lemes <tiago.lemes@windel.com.br>
2022-12-05 13:21:50 -0300

    Corrigido link na sidebar que levava para níveis

---
+++ config/stuff.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../config/connection.php';
+include_once '../functions/html.php';
+include_once '../functions/functions.php';
+
+define('APP_NAME', 'Sistema Financeiro');
+
+define('URL', 'http://localhost/cupcake_software_projects/sistema_financeiro_php_pdo');
+
+define('ADMIN_EMAIL', 'admin@admin.com');
+
+define('ADMIN', 'admin');
